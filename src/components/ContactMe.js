42ba11aeb2ac943dc9bb32fdbf13b6b21dc116a6
funmi7 <talabifunmilayo@gmail.com>
2020-09-06 09:33:45 +0100

    added more bottom space

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import devices from "../utils/devices";
 
 const ContactStyled = styled.div`
-  margin-top: 300vh;
+  margin-top: 370vh;
   height: 90vh;
   padding-top: 15rem;
   position: relative;
